2229008568eabd361b812eb97fd6c0b285c5142d
Alberto Viana <albertogviana@gmail.com>
2014-10-26 19:32:32 -0200

    Fixing phpmd issue

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,33 +1,34 @@
 <?php
 
+namespace AgvValidator;
+
 use Zend\Loader\AutoloaderFactory;
 use Zend\Mvc\Service\ServiceManagerConfig;
 use Zend\ServiceManager\ServiceManager;
 use Zend\Stdlib\ArrayUtils;
-use \RuntimeException;
-
-error_reporting(E_ALL | E_STRICT);
-chdir(__DIR__);
+use RuntimeException;
 
 class Bootstrap
 {
-
     protected static $serviceManager;
     protected static $config;
 
     public static function init()
     {
+        error_reporting(E_ALL | E_STRICT);
+        chdir(__DIR__);
+
         // Load the user-defined test configuration file, if it exists; otherwise, load
-        if (is_readable(dirname(__DIR__) . '/../../config/test.config.php')) {
-            $testConfig = include dirname(__DIR__) . '/../../config/test.config.php';
+        if (is_readable(dirname(__DIR__).'/../../config/test.config.php')) {
+            $testConfig = include dirname(__DIR__).'/../../config/test.config.php';
         } else {
-            throw new \Exception('The file test.config.php doesn\'t exists!' );
+            throw new \Exception('The file test.config.php doesn\'t exists!');
         }
 
-        if (file_exists(dirname(__DIR__) . '/../../config/test.db.config.php')) {
+        if (file_exists(dirname(__DIR__).'/../../config/test.db.config.php')) {
             $testConfig['module_listener_options']['config_static_paths'] = array(
                 //$dbConfig
-                dirname(__DIR__) . '/../../config/test.db.config.php'
+                dirname(__DIR__).'/../../config/test.db.config.php'
             );
         }
 
@@ -42,26 +43,31 @@ class Bootstrap
             }
         }
 
-        $zf2ModulePaths = implode(PATH_SEPARATOR, $zf2ModulePaths) . PATH_SEPARATOR;
-        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ?: (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS : '');
+        $zf2ModulePaths = implode(PATH_SEPARATOR,
+                $zf2ModulePaths).PATH_SEPARATOR;
+        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ? : (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS
+                    : '');
 
         static::initAutoloader();
 
         // use ModuleManager to load this module and it's dependencies
         $baseConfig = array(
             'module_listener_options' => array(
-                'module_paths' => explode(PATH_SEPARATOR, $zf2ModulePaths),
+                'module_paths' => explode(PATH_SEPARATOR,
+                    $zf2ModulePaths),
             ),
         );
 
-        $config = ArrayUtils::merge($baseConfig, $testConfig);
+        $config = ArrayUtils::merge($baseConfig,
+                $testConfig);
 
         $serviceManager = new ServiceManager(new ServiceManagerConfig());
-        $serviceManager->setService('ApplicationConfig', $config);
+        $serviceManager->setService('ApplicationConfig',
+            $config);
         $serviceManager->get('ModuleManager')->loadModules();
 
-//        static::$serviceManager = $serviceManager;
-        static::$config = $config;
+        \Core\Test\Bootstrap::setConfig($config);
+        \Core\Test\Bootstrap::setServiceManager($serviceManager);
     }
 
     public static function getServiceManager()
@@ -78,23 +84,24 @@ class Bootstrap
     {
         $vendorPath = static::findParentPath('vendor');
 
-        if (is_readable($vendorPath . '/autoload.php')) {
-            $loader = include $vendorPath . '/autoload.php';
+        if (is_readable($vendorPath.'/autoload.php')) {
+            include $vendorPath.'/autoload.php';
         } else {
-            $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
+            $zf2Path = getenv('ZF2_PATH') ? : (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath.'/ZF2/library') ? $vendorPath.'/ZF2/library'
+                            : false));
 
             if (!$zf2Path) {
                 throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
             }
 
-            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+            include $zf2Path.'/Zend/Loader/AutoloaderFactory.php';
         }
 
         AutoloaderFactory::factory(array(
             'Zend\Loader\StandardAutoloader' => array(
                 'autoregister_zf' => true,
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__.'/'.__NAMESPACE__,
                 ),
             ),
         ));
@@ -104,21 +111,21 @@ class Bootstrap
     {
         $dir = __DIR__;
         $previousDir = '.';
-        while (!is_dir($dir . '/' . $path)) {
+        while (!is_dir($dir.'/'.$path)) {
             $dir = dirname($dir);
-            if ($previousDir === $dir)
+            if ($previousDir === $dir) {
                 return false;
+            }
             $previousDir = $dir;
         }
 
-        return $dir . '/' . $path;
+        return $dir.'/'.$path;
     }
 
     public static function getModulePath()
     {
         return dirname(__DIR__);
     }
-
 }
 
-Bootstrap::init();
+\AgvValidator\Bootstrap::init();
